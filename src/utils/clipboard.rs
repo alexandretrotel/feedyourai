0615329e35cf44579ad863164da082d13b4d210e
atrtde <trotelalexandre@proton.me>
2026-02-17 19:48:43 -0500

    fix: extract clipboard in utils/ folder

--- src/utils/clipboard.rs
+++ src/utils/clipboard.rs
@@ -5,7 +5,6 @@ use std::path::Path;
 
 use crate::error::{AppError, AppResult};
 
-/// Copies the contents of the specified file to the system clipboard.
 pub fn copy_to_clipboard(output_path: &Path) -> AppResult<()> {
     let mut output_contents = String::new();
     File::open(output_path)?.read_to_string(&mut output_contents)?;
